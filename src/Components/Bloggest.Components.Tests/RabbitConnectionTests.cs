fd108b1f7d790e2786cfbda699db78da3b7885fa
kellhusCorp <kellhus.corp@gmail.com>
2024-09-07 04:01:00 +0400

    6. Рефакторинг тестов.

--- src/Components/Bloggest.Components.Tests/RabbitConnectionTests.cs
+++ src/Components/Bloggest.Components.Tests/RabbitConnectionTests.cs
@@ -8,18 +8,21 @@ namespace Bloggest.Components.Tests;
 
 public class RabbitConnectionTests
 {
-    private readonly RabbitMqContainer _rabbitMq = new RabbitMqBuilder()
-        .WithHostname("localhost")
-        .WithUsername("guest")
-        .WithPassword("guest")
-        .WithPortBinding(54614, 5672)
-        .Build();
+    private RabbitMqContainer _rabbitMq;
+
+    private ILogger<RabbitConnection> _logger;
 
     private IConnectionFactory _rabbitConnectionFactory;
     
     [SetUp]
     public async Task SetUp()
     {
+        _rabbitMq = new RabbitMqBuilder()
+            .WithHostname("localhost")
+            .WithUsername("guest")
+            .WithPassword("guest")
+            .WithPortBinding(54614, 5672)
+            .Build();
         await _rabbitMq.StartAsync();
 
         _rabbitConnectionFactory = new ConnectionFactory
@@ -29,14 +32,14 @@ public class RabbitConnectionTests
             Password = "guest",
             Port = 54614
         };
+        
+        _logger = Mock.Of<ILogger<RabbitConnection>>();
     }
 
     [Test]
     public void Test_RabbitConnection_TryConnect_Returns_True_When_Connection_Is_Established()
     {
-        var logger = new Mock<ILogger<RabbitConnection>>();
-
-        using RabbitConnection rabbitConnection = new RabbitConnection(_rabbitConnectionFactory, logger.Object);
+        using RabbitConnection rabbitConnection = new RabbitConnection(_rabbitConnectionFactory, _logger);
         rabbitConnection.TryConnect();
         
         Assert.That(rabbitConnection.IsConnected, Is.True);
@@ -45,9 +48,7 @@ public class RabbitConnectionTests
     [Test]
     public async Task Test_RabbitConnection_IsConnected_Returns_False_When_Connection_IsShutdown()
     {
-        var logger = new Mock<ILogger<RabbitConnection>>();
-
-        using RabbitConnection rabbitConnection = new RabbitConnection(_rabbitConnectionFactory, logger.Object);
+        using RabbitConnection rabbitConnection = new RabbitConnection(_rabbitConnectionFactory, _logger);
         rabbitConnection.TryConnect();
         
         Assert.That(rabbitConnection.IsConnected, Is.True);
@@ -57,10 +58,31 @@ public class RabbitConnectionTests
         Assert.That(rabbitConnection.IsConnected, Is.False);
     }
     
+    [Test]
+    public void Test_RabbitConnection_CreateModel_Returns_Model_When_Connection_Is_Established()
+    {
+        using RabbitConnection rabbitConnection = new RabbitConnection(_rabbitConnectionFactory, _logger);
+        rabbitConnection.TryConnect();
+        
+        var model = rabbitConnection.CreateModel();
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.IsOpen, Is.True);
+        });
+    }
+    
+    [Test]
+    public void Test_RabbitConnection_CreateModel_Throws_NullReferenceException_When_Connection_Is_Null()
+    {
+        using RabbitConnection rabbitConnection = new RabbitConnection(_rabbitConnectionFactory, _logger);
+        
+        Assert.Throws<NullReferenceException>(() => rabbitConnection.CreateModel());
+    }
+
     [TearDown]
     public async Task TearDown()
     {
         await _rabbitMq.StopAsync();
-        await _rabbitMq.DisposeAsync();
     }
 }
